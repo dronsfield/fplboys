8f9ce8148516f1bf3d9aefbb4bfb1ef9b9360bcf
Josh Dronsfield <josh.dronsfield@gmail.com>
2021-08-12 12:45:30 +0100

    mod: change order of intro paras

--- 2122/src/views/Intro.tsx
+++ 2122/src/views/Intro.tsx
@@ -16,17 +16,17 @@ const Intro: React.FC<{}> = (props) => {
       </p>
       <p>Buy-in options are: £5, £10, £20, or £40.</p>
       <p>
-        It's semi-complicated so I've created this page to help demonstrate how
-        the prize calculations work. The maths is similar to what happens when
-        multiple people go all-in in poker with different amounts of money.
-        Although it's slightly more complex because instead of winner-takes-all
-        there's a prize distribution of 50%/30%/20% for 1st/2nd/3rd.
-      </p>
-      <p>
         Auto join link: <a href={AUTO_JOIN_LINK} children={AUTO_JOIN_LINK} />
       </p>
       <p>
         Payment link: <a href={MONZO_LINK} children={MONZO_LINK} />
+      </p>
+      <p>
+        The prize calculations are semi-complicated so I've created this page to
+        help demonstrate how it works. The maths is similar to what happens when
+        multiple people go all-in in poker with different amounts of money.
+        Although it's slightly more complex because instead of winner-takes-all
+        there's a prize distribution of 50%/30%/20% for 1st/2nd/3rd.
       </p>
     </Section>
   )
