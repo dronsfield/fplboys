727f0882880d4a1b861277508c1940fcfcf28838
Josh Dronsfield <josh.dronsfield@gmail.com>
2021-08-21 13:25:07 +0100

    fix always overflowing x on desktop

--- 2122/src/style/global.tsx
+++ 2122/src/style/global.tsx
@@ -12,7 +12,8 @@ export const GlobalStyle = createGlobalStyle`
 
   body {
     overflow-y: scroll;
-    width: 100vw;
+    width: 100%;
+    max-width: 100%;
     height: 100%;
     color: ${colors.text};
     font-family: IBM Plex Sans, sans-serif;
