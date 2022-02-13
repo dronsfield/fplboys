b636ceee7eb00dca63f70b353ede92b672b71a55
Josh Dronsfield <josh.dronsfield@gmail.com>
2022-02-13 23:29:00 +0000

    add captain count table

--- 2122/src/views/Captains.tsx
+++ 2122/src/views/Captains.tsx
@@ -1,11 +1,14 @@
 import React from "react"
 import { ManagerCell } from "src/components/CommonCells"
 import Section from "src/components/Section"
+import Spacer from "src/components/Spacer"
 import Table from "src/components/Table"
 import { useLeagueContext } from "src/LeagueContext"
+import { sortBy } from "src/util/sortBy"
 import Skeleton from "./Skeleton"
 
 const headers = ["manager", "captain", "vice"] as const
+const aggregateHeaders = ["player", "count"] as const
 
 const Captains: React.FC<{}> = (props) => {
   const { managers, currentEventId, setManagers, players } = useLeagueContext()
@@ -30,9 +33,40 @@ const Captains: React.FC<{}> = (props) => {
     })
   }, [managers])
 
+  const aggregateData = React.useMemo(() => {
+    const captainCounts: { [name: string]: number } = {}
+    data.forEach((item) => {
+      captainCounts[item.captain] = (captainCounts[item.captain] || 0) + 1
+    })
+    const countData = Object.keys(captainCounts).map((player) => {
+      return { player, count: captainCounts[player] }
+    })
+    const sortedCountData = sortBy(countData, "count", true)
+    return sortedCountData
+  }, [data])
+
   return (
     <Skeleton>
+      <Table
+        data={aggregateData}
+        headers={aggregateHeaders}
+        renderCell={(header, rowData) => {
+          return rowData[header]
+        }}
+        renderHeader={(header) => {
+          if (header === "count") {
+            return "Captain count"
+          } else {
+            return header
+          }
+        }}
+        cellWidths={{
+          player: ["auto"],
+          count: ["auto"]
+        }}
+      />
       <Section>
+        <Spacer height={16} />
         <Table
           data={data}
           headers={headers}
